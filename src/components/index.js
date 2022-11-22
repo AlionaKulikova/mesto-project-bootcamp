cf2fe7cd57d4bb41beaeb7a0cd2d0c8184e28d73
Aliona Kulikova <kulikovaaliona888@yandex.ru>
2022-11-22 08:57:53 +0300

    8sprint

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,17 @@
 import '../pages/index.css';
-import { createProfile, openModalWindow, closeModalWindow, openEditProfile } from "./modal.js";
-import { likes, deleteCard, createCard, createArrayCards, createNewCard } from "./card.js";
-import { validFormProfile, validFormNewMesto } from "./validate.js";
+import { createProfile, openModalWindow, closeModalWindow, openEditProfile, openPhotoProfile, createPhotoProfile } from "./modal.js";
+import { likes, deleteCard, createArrayCards, createNewCard } from "./card.js";
+import { validFormProfile, validFormNewMesto, validFormProfilePhoto } from "./validate.js";
 import { openPopupPicture, openPopup, closePopup, getSrcPicture } from "./utils.js";
+
 createArrayCards();
 openEditProfile();
+createPhotoProfile();
 closeModalWindow();
 openModalWindow();
 createProfile();
 createNewCard();
+openPhotoProfile();
 validFormProfile();
 validFormNewMesto();
+validFormProfilePhoto();
--- src/components/validate.js
+++ src/components/validate.js
@@ -205,3 +205,94 @@ export function validFormNewMesto() {
     return "Введите адрес сайта";
   }
 }
+
+export function validFormProfilePhoto() {
+  const formPhoto = document.querySelector("#form-photo");
+  const validatorsPhoto = {
+    //nameImage: validateNameCard,
+    addLinkPhoto: validateLinkPhoto,
+  };
+  const inputGroupsPhoto = {
+   // nameImage: "#nameCard",
+    addLinkPhoto: "#linkCardPhoto",
+  };
+
+  formPhoto.addEventListener("input", (e) => {
+    const key = e.target.name;
+    const value = e.target.value;
+    const formData = new FormData(e.currentTarget);
+    const values = Object.fromEntries(formData);
+    const error = validatePhoto(key, value, values);
+    if (error || !!error) {
+      setErrorPhotoCard(key, error);
+    } else {
+      removeErrorPhotoCard(key);
+    }
+
+    let isFormValidPhoto = true;
+    formData.forEach((value, key) => {
+      const error = validatePhoto(key, value, values);
+      if (!error) {
+        return;
+      }
+      setErrorPhotoCard(key, error);
+      isFormValidPhoto = false;
+    });
+    if (!isFormValidPhoto) {
+      const buttonCreate = document.querySelector("#edit");
+      buttonCreate.setAttribute("disabled", true);
+      buttonCreate.classList.add("form__submit-button_color_noactive");
+      return;
+    }
+    const buttonCreate = document.querySelector("#edit");
+    buttonCreate.removeAttribute("disabled");
+    buttonCreate.classList.remove("form__submit-button_color_noactive");
+    return;
+  });
+  formPhoto.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buttonCreate.setAttribute("disabled", true);
+    buttonCreate.classList.add("form__submit-button_color_noactive");
+  });
+
+  function setErrorPhotoCard(key, errorMessage) {
+    const inputGroupPhoto = inputGroupsPhoto[key];
+    const element = formPhoto.querySelector(inputGroupPhoto);
+    const input = element.querySelector(".form__field_name_text");
+    input.classList.add("form__input_invalid");
+    const error = element.querySelector(".form__error");
+    error.textContent = errorMessage;
+    error.classList.remove("form__error_hidden");
+  }
+
+  function removeErrorPhotoCard(key) {
+    const inputGroupPhoto = inputGroupsPhoto[key];
+    const element = formPhoto.querySelector(inputGroupPhoto);
+    const input = element.querySelector(".form__field_name_text");
+    input.classList.remove("form__input_invalid");
+    const error = element.querySelector(".form__error");
+    error.textContent = null;
+    error.classList.add("form__error_hidden");
+  }
+
+  function validatePhoto(key, value, values) {
+    const validatorPhoto = validatorsPhoto[key];
+    return validatorPhoto(value, values);
+  }
+
+
+  function validateLinkPhoto(value) {
+    if (!value) {
+      return "Вы пропустили это поле";
+    }
+    const input = document.createElement("input");
+    input.type = "url";
+    input.required = true;
+    input.value = value;
+    const isValid = typeof input.checkValidity === "function" ? input.checkValidity() : /\S+@\S+\.\S+/.test(value);
+    if (isValid) {
+      return null;
+    }
+    return "Введите адрес сайта";
+  }
+}
